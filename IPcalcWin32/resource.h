229c91c489e2fac8b196c4d050510a350af8470b
Uliana.29 <Uliana.29@DESKTOP-6DD5K9D>
2024-03-06 17:50:57 +0300

    Add project files.

---
+++ IPcalcWin32/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_DIALOG1                     101
+#define IDC_SPIN_IPADDRESS              1001
+#define IDC_IPADDRESS                   1001
+#define IDC_SPIN_PREFIX                 1002
+#define IDC_IPADDRESS1                  1006
+#define IDC_IPADDRESS2                  1007
+#define IDC_STATIC_INFO                 1009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
